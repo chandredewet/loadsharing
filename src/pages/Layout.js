5933019ff278412a6d29c93dc522f2e0e1ec8377
chandredewet <chandredewet@gmail.com>
2023-06-19 12:47:24 +0200

    navigation started

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import Navigation from "../components/Navigation";
 
 const Layout = () => {
     return (
         <>
-            <Navbar />
+            <Navigation />
             <Outlet />
         </>
     );
