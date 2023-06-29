4e3d89a344c4422ace5edf5f740cc2777390426c
chandredewet <chandredewet@gmail.com>
2023-06-29 11:14:20 +0200

    working on data for calendar

--- client/src/pages/Schedule.js
+++ client/src/pages/Schedule.js
@@ -1,12 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import "./Schedule.css"
-// import BasicCalendar from "../components/BasicCalendar";
+import BasicCalendar from "../components/BasicCalendar";
 
 const Schedule = () => {
 
   const [areaData, setAreaData] = useState([]);
   const [eskomData, setEskomData] = useState([]);  
+
+    
+  let dbEvents = 
+  [{"area_name": "city-of-cape-town-area-11",
+  "start": "2023-06-30T22:00:00+02:00",
+  "finsh": "2023-07-01T00:00:00+02:00",
+  "name":"Chezlin",
+  colour:"blue"},
+  {"area_name": "city-of-cape-town-area-11",
+  "start": "2023-06-30T22:00:00+02:00",
+  "finsh": "2023-07-01T00:00:00+02:00",
+  name:"Chandre De Wet",
+  colour: "pink",}];
+
+  const [myEventsList, setMyEventsList] = useState([]);
+    
+
+    dbEvents.map((outage) => {
+        let clOutage = {
+            start: moment(outage.start).toDate(),
+            end: moment(outage.finsh).toDate(),
+            title: `${outage.name} - ${outage.area_name}`,
+            colour: outage.colour,  
+        }
+        setMyEventsList(myEventsList.concat(clOutage))    ;        
+    })
+
 
   useEffect(() => { 
 
@@ -46,14 +73,18 @@ const Schedule = () => {
      return (
         <Container className="text-center" id="schedule-container" fluid>
             <h1>Test</h1>
-             {areaData.map((el, inx) => {
+             {/* {areaData.map((el, inx) => {
                 return <p key={inx}>{el.user_area_name}</p>;
-            })}
+            })} */}
              {/* {eskomData?.map((obj, inx) => {
                 return <p key={inx}>{obj.length}</p>;
             })}
             {console.log("All the data",eskomData)}
            */}
+           < cleanedEventList />
+           <div style={{ height: "500px" }}>
+             <BasicCalendar myEventsList={myEventsList}/>
+         </div>           
         </Container> 
     );
 }
